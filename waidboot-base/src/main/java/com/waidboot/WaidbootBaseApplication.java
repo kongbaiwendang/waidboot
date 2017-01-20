3b867e0c2c63b1406c22bceaac089be75082de42
kongbaiwendang <aaaassssdddd1pm@sina.com>
2017-01-20 10:26:36 +0800

    springboot基础代码

---
+++ waidboot-base/src/main/java/com/waidboot/WaidbootBaseApplication.java
@@ -0,0 +1,12 @@
+package com.waidboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaidbootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaidbootBaseApplication.class, args);
+	}
+}
