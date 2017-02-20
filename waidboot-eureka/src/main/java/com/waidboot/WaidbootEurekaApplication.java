3cc608f9f037ca74c6f7282107a5f4791cd7fbe4
kongbaiwendang <aaaassssdddd1pm@sina.com>
2017-02-20 17:41:49 +0800

    提交eureka模块和init模板代码

---
+++ waidboot-eureka/src/main/java/com/waidboot/WaidbootEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.waidboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaidbootEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaidbootEurekaApplication.class, args);
+	}
+}
---
+++ waidboot-init/src/main/java/com/waidboot/WaidbootInitApplication.java
@@ -0,0 +1,12 @@
+package com.waidboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaidbootInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaidbootInitApplication.class, args);
+	}
+}
